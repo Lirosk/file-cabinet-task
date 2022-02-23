a210e8cdc08ad1ff7fedd96bd508d46fd37d94f8
Lirosk <kirill_crysis@bk.ru>
2022-02-23 21:30:22 +0300

    Add FileCabinetRecord validation methods.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -12,75 +12,12 @@
             decimal averageMark,
             char classLetter)
         {
-            const int firstNameMinLen = 2;
-            const int firstNameMaxLen = 60;
-            const int lastNameMinLen = 2;
-            const int lastNameMaxLen = 60;
-            DateTime dateOfBirthMinValue = new DateTime(1950, 1, 1);
-            DateTime dateOfBirthMaxValue = DateTime.Now;
-            const short schoolGradeMinValue = 1;
-            const short schoolGradeMaxValue = 11;
-            const decimal averageMarkMinValue = 0m;
-            const decimal averageMarkMaxValue = 10m;
-            const char classLetterMinValue = 'A';
-            const char classLetterMaxValue = 'E';
-
-            if (string.IsNullOrEmpty(firstName))
-            {
-                throw new ArgumentNullException(nameof(firstName));
-            }
-
-            if (string.IsNullOrEmpty(lastName))
-            {
-                throw new ArgumentNullException(nameof(lastName));
-            }
-
-            if (firstName.Length < firstNameMinLen ||
-                firstName.Length > firstNameMaxLen ||
-                firstName.Contains(' ', StringComparison.Ordinal))
-            {
-                throw new ArgumentException(
-                    $"{nameof(firstName)} must contatin " +
-                    $"from {firstNameMinLen} " +
-                    $"to {firstNameMaxLen} characters, " +
-                    "and must not contain spaces.");
-            }
-
-            if (lastName.Length < lastNameMinLen ||
-                lastName.Length > lastNameMaxLen ||
-                lastName.Contains(' ', StringComparison.Ordinal))
-            {
-                throw new ArgumentException(
-                    $"{nameof(lastName)} must contatin " +
-                    $"from {lastNameMinLen} " +
-                    $"to {lastNameMaxLen} characters, " +
-                    "and must not contain spaces.");
-            }
-
-            if (dateOfBirth < dateOfBirthMinValue ||
-                dateOfBirth > dateOfBirthMaxValue)
-            {
-                throw new ArgumentOutOfRangeException(nameof(dateOfBirth));
-            }
-
-            if (schoolGrade < schoolGradeMinValue ||
-                schoolGrade > schoolGradeMaxValue)
-            {
-                throw new ArgumentOutOfRangeException(nameof(schoolGrade));
-            }
-
-            if (averageMark < averageMarkMinValue ||
-                averageMark > averageMarkMaxValue)
-            {
-                throw new ArgumentOutOfRangeException(nameof(averageMark));
-            }
-
-            classLetter = char.ToUpperInvariant(classLetter);
-            if (classLetter < classLetterMinValue ||
-                classLetter > classLetterMaxValue)
-            {
-                throw new ArgumentOutOfRangeException(nameof(classLetter));
-            }
+            FileCabinetRecord.ValidateFirstName(firstName);
+            FileCabinetRecord.ValidateLastName(lastName);
+            FileCabinetRecord.ValidateDateOfBirth(dateOfBirth);
+            FileCabinetRecord.ValidateSchoolGrade(schoolGrade);
+            FileCabinetRecord.ValidateAverageMark(averageMark);
+            FileCabinetRecord.ValidateClassLetter(classLetter);
 
             var record = new FileCabinetRecord
             {
