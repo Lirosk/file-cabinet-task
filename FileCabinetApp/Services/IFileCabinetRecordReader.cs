5c27fd6056985a10ddfb2bcefb21d5f30046ab40
Lirosk <kirill_crysis@bk.ru>
2022-03-02 22:57:58 +0300

    Replace FileCabinetRecordReader with IFileCabinetReader

---
+++ FileCabinetApp/Services/IFileCabinetRecordReader.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+
+namespace FileCabinetApp.Services
+{
+    /// <summary>
+    /// Interface for reading records from stream.
+    /// </summary>
+    public interface IFileCabinetRecordReader
+    {
+        /// <summary>
+        /// Reads all records.
+        /// </summary>
+        /// <returns>All readed records.</returns>
+        public IList<FileCabinetRecord> ReadAll();
+    }
+}
